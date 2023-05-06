e7d041c494e0d8513baebeed85fc06fd2d219b71
pdfkpb <pdfkpb@gmail.com>
2023-05-06 13:09:52 -0700

    something

--- lib/ClawControl/fingers.cpp
+++ lib/ClawControl/fingers.cpp
@@ -1,9 +1,21 @@
 #include "fingers.h"
 
-Fingers::Fingers() {
+Fingers::Fingers(int vertPin, int horPin) {
+    m_set1Pin = nsPin;
+    m_set2Pin = ewPin;
 
+    m_state = State::Initialize;
 }
 
 Fingers::~Fingers() {
-    
+}
+
+void Fingers::next() {
+    switch(m_state) {
+    case Initialize:
+        break;
+    case Set1Vertical:
+        break;
+    case
+    }
 }
--- lib/ClawControl/fingers.h
+++ lib/ClawControl/fingers.h
@@ -3,10 +3,14 @@
 
 class Fingers {
 public:
-    Fingers();
+    Fingers(int vertPin, int horPin);
     ~Fingers();
 
 private:
+    int m_set1Pin;
+    int m_set2Pin;
+
+    int m_state;
 };
 
 #endif // FINGERS_H
