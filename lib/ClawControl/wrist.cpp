4e7de726d636e266f8ef7d8e00dbd4f93a4a759f
Kevin Barnett <pdfkpb@gmail.com>
2023-05-13 10:25:33 -0700

    most of the wrist pio work is done.

--- lib/ClawControl/wrist.cpp
+++ lib/ClawControl/wrist.cpp
@@ -5,9 +5,12 @@
 #include <stdio.h>
 #include "pico/time.h"
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
 
-Wrist::Wrist(int stepPin, int directionPin, int sleepPin, int homingPin) {
+Wrist::Wrist(
+    uint32_t stepPin,
+    uint32_t directionPin,
+    uint32_t sleepPin,
+    uint32_t homingPin) {
     // Save Pin Info
     m_stepPin = stepPin;
     m_directionPin = directionPin;
@@ -15,9 +18,10 @@ Wrist::Wrist(int stepPin, int directionPin, int sleepPin, int homingPin) {
     m_homingPin = homingPin;
 
     // Initialize Pins
-    // This one is probably wrong, it needs to be a PWM pin
-    gpio_init(m_stepPin);
-    gpio_set_dir(m_stepPin, GPIO_OUT);
+    m_stepperConfig.pio = (pioBlockCounter++ % 2) ? pio0 : pio1;
+    m_stepperConfig.stateMachine = pio_claim_unused_sm(m_stepperConfig.pio, true);
+    m_stepperConfig.programOffset = pio_add_program(m_stepperConfig.pio, &stepper_program);
+    stepper_program_init(m_stepperConfig.pio, m_stepperConfig.stateMachine, m_stepperConfig.programOffset, m_stepPin);
 
     gpio_init(m_directionPin);
     gpio_set_dir(m_directionPin, GPIO_OUT);
@@ -29,11 +33,6 @@ Wrist::Wrist(int stepPin, int directionPin, int sleepPin, int homingPin) {
     gpio_init(m_homingPin);
     gpio_set_dir(m_homingPin, GPIO_IN);
     gpio_pull_up(m_homingPin);
-
-    // Set Some Defaults
-    m_angle = 0;
-    m_speed = 0.0;
-    m_direction = Direction::CW;
 }
 
 Wrist::~Wrist() {
@@ -41,14 +40,11 @@ Wrist::~Wrist() {
 }
 
 void Wrist::home() {
-    // Hold for reseting at the end
-    float tmpSpeed = m_speed;
-
     // Begin Homing
-    m_speed = HOMING_SPEED;
+    setSpeed(HOMING_SPEED);
     gpio_put(m_directionPin, Direction::CW);
 
-    enable();
+    
     while (gpio_get(m_homingPin)) {
         sleep_ms(HOMING_POLL_MS);
     }
@@ -60,7 +56,7 @@ void Wrist::home() {
     sleep_ms(HOMING_POLL_MS);
     disable();
 
-    m_speed /= 2;
+    setSpeed(HOMING_SPEED / 2);
     gpio_put(m_directionPin, Direction::CW);
 
     while(gpio_get(m_homingPin)) {
@@ -68,42 +64,28 @@ void Wrist::home() {
     }
     disable();
     // End Homing
-
-    // Reset
-    m_speed = tmpSpeed;
 }
 
 /**
  * @brief Sets the Angle and 
  * 
- * @param deg 
+ * @param angle - in degrees
  */
-void Wrist::turn(float deg) {
-    m_angle += deg;
-    
+void Wrist::turn(int32_t angle) {
+    gpio_put(m_directionPin, (angle > 0 ? Direction::CW : Direction::CCW));
+
     enable();
 
-    uint32_t steps = angle2Steps(m_angle);
-    pio_sm_put_blocking(pio, sm, steps);
+    uint32_t steps = angle2Steps(angle);
+    pio_sm_put_blocking(m_stepperConfig.pio, m_stepperConfig.stateMachine, (FUNC_STEPPER_TURN & steps));
 }
 
 
-bool Wrist::setSpeed(float speed) {
-    if (speed >= 0 && speed < 1) {
-        m_speed = speed;
-
-        pio_sm_set_enabled(m_pio, m_sm, false);
-        pio_sm_put_blocking(m_pio, m_sm, period);
-        pio_sm_exec(m_pio, m_sm, pio_encode_pull(false, false));
-        pio_sm_exec(m_pio, m_sm, pio_encode_out(pio_isr, 32));
-
-        return true;
+void Wrist::setSpeed(float speed) {
+    if (speed = 0 && speed <= 1) {
+        // Come back to this function
+        // pio_sm_put_blocking(pio, sm, (FUNC_STEPPER_TURN & ));
     }
-    return false;
-}
-
-void Wrist::setDirection(Direction direction) {
-    m_direction = direction;
 }
 
 /**
@@ -112,11 +94,13 @@ void Wrist::setDirection(Direction direction) {
  * @return true - We all good
  * @return false - If there is no response from the SM or there is an error response
  */
-bool Writst::isReady() {
-    return !pio_sm_is_rx_fifo_empty(m_pio, m_sm) && !(m_err = pio_sm_get(m_pio, m_sm));
+bool Wrist::isReady() {
+    return !pio_sm_is_rx_fifo_empty(m_stepperConfig.pio, m_stepperConfig.stateMachine) && !pio_sm_get(m_stepperConfig.pio, m_stepperConfig.stateMachine);
 }
 
-// Private Functions
+// ------ Private ------ //
+
+uint32_t Wrist::pioBlockCounter = 0;
 
 /**
  * @brief brings the sleep pin high, costing some power, but at least we can turn
@@ -134,12 +118,12 @@ void Wrist::disable() {
     gpio_put(m_sleepPin, 0);
 }
 
-// Write `period` to the input shift register
-void Wrist:(PIO pio, uint sm, uint32_t period) {
-    
-}
-
-// Write `level` to TX FIFO. State machine will copy this into X.
-void Wrist::pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
-    pio_sm_put_blocking(pio, sm, level);
+/**
+ * @brief Converts the given angle in Degrees to Steps based on Resolution
+ * 
+ * @param angle - angle in degrees
+ * @return uint32_t - the number of steps that correlates to
+ */
+uint32_t Wrist::angle2Steps(uint32_t angle) {
+    return static_cast<uint32_t>(angle / (DEG_PER_STEP * STEP_RESOLUTION));
 }
--- lib/ClawControl/wrist.h
+++ lib/ClawControl/wrist.h
@@ -1,45 +1,58 @@
 #ifndef WRIST_H
 #define WRIST_H
 
-#include "hardware/irq.h"
+#include "hardware/pio.h"
 
-#define HOMING_SPEED 0.2
-#define HOMING_POLL_MS 100
+#define HOMING_SPEED   0.2f
+#define HOMING_POLL_MS 100u
 
-enum Direction {
-    CW,
-    CCW
-};
+#define HALF_STEP            0.5f
+#define QUARTER_STEP         0.25f
+#define EIGTH_STEP           0.125f
+#define THIRTY_SECONDTH_STEP 0.03125f
+
+#define DEG_PER_STEP         1.8f
+#define STEP_RESOLUTION      QUARTER_STEP
+#define STEPS_PER_REVOLUTION (360 / (DEG_PER_STEP * STEP_RESOLUTION))
 
 class Wrist {
 public:
-    Wrist(int stepPin, int directionPin, int sleepPin, int homingPin);
+    Wrist(
+        uint32_t stepPin,
+        uint32_t directionPin,
+        uint32_t sleepPin,
+        uint32_t homingPin);
     ~Wrist();
 
     void home();
 
-    void turn(float deg);
-    bool setSpeed(float speed); // [0.0, 1.0)
-    void setDirection(Direction direction);
+    void turn(int32_t deg);
+    void setSpeed(float speed); // (0.0, 1.0]
 
-    bool pollState();
+    bool isReady();
 
 private:
-    int m_stepPin;
-    int m_directionPin;
-    int m_sleepPin;
-    int m_homingPin;
+    static uint32_t pioBlockCounter;
 
-    int m_currentAngle;
-    float m_angle;
-    float m_speed;
-    Direction m_direction;
+    uint32_t m_stepPin;
+    uint32_t m_directionPin;
+    uint32_t m_sleepPin;
+    uint32_t m_homingPin;
 
     void enable();
     void disable();
+    uint32_t angle2Steps(uint32_t angle);
 
-    void pio_pwm_set_period(PIO pio, uint sm, uint32_t period);
-    void pio_pwm_set_level(PIO pio, uint sm, uint32_t level);
+    struct StepperController {
+        PIO pio;
+        uint32_t stateMachine;
+        uint32_t programOffset;
+    } m_stepperConfig;
+
+    enum Direction {
+        CW,
+        CCW
+    };
 };
 
 #endif // WRIST_H
